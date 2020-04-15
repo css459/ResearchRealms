9e9650853c80b5cc48cce1dcf71eec8aa7b37bda
Cole Smith <css@nyu.edu>
2020-04-15 16:22:14 -0400

    Working discord interface

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ResearchRealms
 A Slack and Discord Bot for Quantitative Research in Python
+
+[Add this bot to your discord server!](https://discordapp.com/api/oauth2/authorize?client_id=700004199844282369&permissions=593024&scope=bot)
--- discord_bot.py
+++ discord_bot.py
@@ -1,1 +1,17 @@
 # Executable for Discord Interface
+import os
+
+from discord.ext import commands
+
+TOKEN = os.getenv('TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.command(name='echo', help='Tests the bot by echoing back what is said')
+async def bot_echo(ctx):
+    # print(ctx)
+    await ctx.send(" ".join(ctx.message.content.split()[1:]))
+
+
+bot.run(str(TOKEN))
